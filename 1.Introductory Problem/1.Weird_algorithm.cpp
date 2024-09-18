6b9aceded62b8c954441a2f5237702b109242b42
SANYASI-RAJA <sanyasiraja001@gmail.com>
2024-09-18 14:54:43 +0530

    first

---
+++ 1.Introductory Problem/1.Weird_algorithm.cpp
@@ -0,0 +1,423 @@
+//                                               ॐ भूर्भुव: स्व: तत्सवितुर्वरेण्यं भर्गो देवस्य धीमहि धियो यो न: प्रचोदयात्।
+
+
+
+//                                                                                                  san
+//                                                                                                sanyasi
+//                                                                                              sanyasiraja
+//                                                                                                sanyasi
+//                                                            sanyasisanyasi                        san                      sanyasisanyasisan
+//                                                                sanyasisanyasis                                      sanyasisanyasisan
+//                                                                     sanyasisanyasisanyas                      sanyasisanyasisanyas
+//                                                                          sanyasisanyasisanyas            sanyasisanyasisanyas
+//                                                                                 sanyasisanyasisanyasisanyasisanyasisanya
+//                                                                                         sanyasisanyasisanyasisany
+//                                                    sanyasisanyasisan
+//                                                sanyasisanyasisanyasisany
+//                                            sanyasisanyasisanyas    sanyasi
+//                                        sanyasisanyasisanyas         sanyasi
+//                                    sanyasisanyasisanyas               sanyasi
+//                                 sanyasisanyasisan                    sanyasi
+//                                                                       sanyasi
+//                                                                       sanyasi
+//                                                                      sanyasi                            sanyasisanyasisan
+//                                                                     sanyasi                        sanyasisanyasisanyasisanyas
+//                                                                    sanyasi                     sanyasisanyasisanyasisanyasisanyansi
+//                                                                   sanyasi                  sanyasisanyasissanyasisanyasisanyasisanyasisan
+//                                                                  sanyasi                 sanyasisanyasis                  sanyasisanyasisan
+//                                                                sanyasi                 sanyasisanyasis                         sanyasisanyasis
+//                                                              sanyasi                sanyasisanyasis                                sanyasisanyas
+//                                                           sanyasisan              sanyasisanyasis                                    sanyasisanyas
+//                                                        sanyasisany              sanyasisanyasis                                        sanyasisanyas
+//                                                     sanyasisanya              sanyasisanyasis                                            sanyasisanyas
+//                                                  sanyasisanyasi              sanyasisanyasis                                               sanyasisanya
+//                                                  sanyasisanyasi             sanyasisanyasis                                                  sanyasisany
+//                                                     sanyasisanya           sanyasisanyasis                                                    sanyasisany
+//                                                       sanyasisany         sanyasisanyasis                                                      sanyasisany
+//                                                          sanyasisa       sanyasisanyasisa                                                      sanyasisany
+//                                                             sanyasi                                                                            sanyasisany
+//                                                               sanyasi                     sany                                                 sanyasisany
+//                                                                 sanyasi                   sany                                                sanyasisany
+//                                                                   sanyasi                 sany                                               sanyasisanya
+//                                                                      sanyasi              sanya                                             sanyasisanya
+//                                                                        sanyasi             sanya                                           sanyasisanya
+// sanyasi                                                                 sanyasi             sanyas                                        sanyasisanya
+//  sanyasi                                                                  sanyasi            sanyas                                      sanyasisanya
+//   sanyasis                                                                sanyasi             sanyasi                                   sanyasisanya
+//     sanyasisa                                                              sanyasi             sanyasi                                 sanyasisanya
+//       sanyasisa                                                             sanyasi              sanyasisa                            sanyasisanya
+//         sanyasisa                                                          sanyasi                 sanyasisany                      sanyasisanya
+//           sanyasisa                                                       sanyasi                    sanyasisanya                sanyasisanyas
+//              sanyasisa                                                  sanyasi                        sanyasisanyasisanyasisanyasisanyasisa
+//                 sanyasisany                                          sanyasisa                            sanyasisanyasisanyasisanyasisany
+//                    sanyasisanya                                   sanyasisan                                 sanyasisanyasisanyasisanyas
+//                       sanyasisanyasi                            sanyasisan                                       sanyasisanyasisanyas
+//                          sanyasisannyasi                   sanyasisanya
+//                              sanyasisanyasisany          sanyasisany
+//                                  sanyasisanyasisanyasisanyasisanya
+//                                      sanyasisanyasisanyasisanya
+//                                           sanyasisanyasisan
+
+
+
+
+
+//                                              ॐ त्र्यम्बकं यजामहे सुगन्धिं पुष्टिवर्धनम्। उर्वारुकमिव बन्धनान् मृत्योर्मुक्षीय मामृतात्॥
+
+
+
+
+//                                                                                                                                .
+// //  ________          .         .                        .       ________   _____ _____            ___           .        ___________      .
+// // |                 / \        |\     |   \     /      / \     |                |                |    \        / \            |          / \
+// // |                /   \       | \    |    \   /      /   \    |                |                |     |      /   \           |         /   \
+// // |_______        /     \      |  \   |     \ /      /     \   |________        |                |  __/      /     \          |        /     \
+// //         |      /_______\     |   \  |      |      /_______\           |       |                |  \       /_______\         |       /_______\
+// //         |     /         \    |    \ |      |     /         \          |       |                |   \     /         \   \    /      /         \
+// //  _______|    /           \   |     \|      |    /           \ ________|  _____|_____           |    \   /           \   \__/      /           \
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+#define int ll
+#define endl "\n"
+#define sp " "
+#define pb push_back
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define sz size()
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pi pair<int,int>
+#define vpi vector<pi>
+#define mii map<int,int>
+#define umii unordered_map<int,int>
+#define pqi priority_queue<int>
+#define pqig priority_queue<int,vi,greater<int>>
+#define pqpii prority_queue<pi>
+#define tp3(T) tuple<T,T,T>
+#define rep(i,a,b) for (int i = a; i < b; ++i)
+#define _rep(i,a,b) for (int i = a; i <= b; ++i)
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define srt(a) sort(all(a))
+#define unique(a) a.resize(unique(all(a))-a.begin())
+#define mx_ele(a) *max_element(all(a))
+#define mn_ele(a) *min_element(all(a))
+#define _down(x) (int)floor(1.0*x)
+#define debug(a) cout << #a << " = " << a << endl;
+#define debug2(a,b) cout<<#a<<" = "<<a<<' '<<#b<<" = "<<b<<endl;
+#define debugx(a, idx) cout << #a << "[" << idx << "] = " << a[idx] << endl;
+#define debugpii(a) cout<<#a<<" = "<<a.first<<", "<<a.second<<endl;
+#define debugarr(a, x) cout<<#a<<": "; for(int i = 0; i < x; i++) cout << a[i] << ' '; cout << endl;
+#define debugarr2(a, n, m) cout<<#a<<": \n";for(int i=0;i<n;i++){for(int j=0;j<m;j++)cout<<a[i][j]<<' ';cout<<'\n';}
+#define debugvec(a) cout<<#a<<": "; for(int i = 0; i < a.size(); i++) cout << a[i] << ' '; cout << endl;
+#define indexed_set tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>
+#define ifyn  if(temp==0) cout<<"NO"<<endl;else cout<<"YES"<<endl;
+const int M = 1e9 + 7;
+
+int modM( int n, int m ) { return (  (n % M) * (m % M) ) % M; }
+int modA( int n, int m ) { return ( (n % M) + (m % M) ) % M; }
+int modS( int n, int m ) { return ( ( (n % M) - (m % M) ) + M) % M; }
+
+template<typename L, typename R>
+ostream& operator<<(ostream& out, const pair<L, R>& p) {
+    out << '(' << p.first << ',' << p.second << ')';
+    return out;
+}
+
+template<typename T1, typename T2, typename T3>
+ostream& operator<<(ostream& out, const tuple<T1, T2, T3>& tp) {
+    auto &[t1, t2, t3] = tp;
+    out << '(' << t1 << ',' << t2 << ',' << t3 << ')';
+    return out;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& v) {
+    for (auto &i : v) out << i << ' ';
+    out << endl;
+    return out;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const set<T>& v) {
+    for (auto &i : v) out << i << ' ';
+    out << endl;
+    return out;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const unordered_set<T>& v) {
+    for (auto &i : v) out << i << ' ';
+    out << endl;
+    return out;
+}
+
+template<typename K, typename V>
+ostream& operator<<(ostream& out, const map<K, V>& m) {
+    out << '[';
+    for (auto &[k, v] : m) {
+        out << k << ':' << v << sp;
+    }
+    out << "]\n";
+    return out;
+}
+
+template<typename K, typename V>
+ostream& operator<<(ostream& out, const unordered_map<K, V>& m) {
+    out << '[';
+    for (auto &[k, v] : m) {
+        out << k << ':' << v << sp;
+    }
+    out << "]\n";
+    return out;
+}
+
+struct Fenwick {
+    int n;
+    vector<int>bit;
+    Fenwick(int a)
+    {
+        n = a;
+        bit.resize(n + 1);
+    }
+    void update(int ind, int val)
+    {
+        for (; ind <= n; ind += (ind & (-ind)))
+            bit[ind] += val;
+    }
+
+    int query(int ind)
+    {
+        int res = 0;
+        for (; ind; ind -= (ind & (-ind)))
+            res += bit[ind];
+        return res;
+    }
+
+};
+
+class SGTree {
+    vector<int>seg;
+public:
+    SGTree(int n)
+    {
+        seg.resize(4 * n);
+    }
+
+    void build (int ind, int low, int high, vector<int>&arr)
+    {
+        if (low == high)
+        {
+            seg[ind] = arr[low];
+            return;
+        }
+
+        int mid = (low + high) >> 1;
+        build(2 * ind + 1, low, mid, arr);
+        build(2 * ind + 2, mid + 1, high, arr);
+        seg[ind] = (seg[2 * ind + 1] + seg[2 * ind + 2]);
+    }
+
+    int query(int ind, int low, int high, int l, int r)
+    {
+        if (r < low || high < l)return 0;
+
+        if (l <= low && high <= r)return seg[ind];
+
+        int mid = (low + high) >> 1;
+        int left = query(2 * ind + 1, low, mid, l, r);
+        int right = query(2 * ind + 2, mid + 1, high, l, r);
+        return (left + right);
+    }
+
+    void update(int ind, int low, int high, int i, int val)
+    {
+        if (low == high)
+        {
+            seg[ind] = val;
+            return;
+        }
+
+        int mid = (low + high) >> 1;
+        if (i <= mid)update(2 * ind + 1, low, mid, i, val);
+        else update(2 * ind + 2, mid + 1, high, i, val);
+        seg[ind] = (seg[2 * ind + 1] + seg[2 * ind + 2]);
+    }
+
+};
+
+
+
+
+class Lazy_ST {
+
+public:
+    vector<int>seg, lazy;
+    Lazy_ST(int n)
+    {
+        seg.resize(4 * n + 1);
+        lazy.resize(4 * n + 1);
+
+    }
+public:
+    void build(int ind, int low, int high, vector<int>&arr)
+    {
+        if (low == high)
+        {
+            seg[ind] = arr[low];
+            return;
+        }
+
+        int mid = (low + high) >> 1;
+        build(2 * ind + 1, low, mid, arr);
+        build(2 * ind + 2, mid + 1, high, arr);
+        seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
+    }
+public:
+    void update(int ind, int low, int high, int l, int r, int val)
+    {
+
+        if (lazy[ind] != 0)
+        {
+            seg[ind] += (high - low + 1) * lazy[ind];
+
+            if (low != high)
+            {
+                lazy[2 * ind + 1] += lazy[ind];
+                lazy[2 * ind + 2] += lazy[ind];
+
+            }
+            lazy[ind] = 0;
+        }
+
+        if (high < l || r < low)
+        {
+            return;
+        }
+        if (low >= l && high <= r)
+        {
+            seg[ind] += (high - low + 1) * val;
+
+            if (low != high)
+            {
+                lazy[2 * ind + 1] += val;
+                lazy[2 * ind + 2] += val;
+            }
+            return;
+        }
+
+        int mid = (low + high) >> 1;
+        update(2 * ind + 1, low, mid, l, r, val);
+        update(2 * ind + 2, mid + 1, high, l, r, val);
+        seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
+    }
+
+public:
+    int query(int ind, int low, int high, int l, int r)
+    {
+
+        if (lazy[ind] != 0)
+        {
+            seg[ind] += (high - low + 1) * lazy[ind];
+
+            if (low != high)
+            {
+                lazy[2 * ind + 1] += lazy[ind];
+                lazy[2 * ind + 2] += lazy[ind];
+
+            }
+            lazy[ind] = 0;
+        }
+
+        if (high < l || r < low)
+            return 0;
+
+        if (low >= l && high <= r)return seg[ind];
+
+        int mid = (low + high) >> 1;
+        int left = query(2 * ind + 1, low, mid, l, r);
+        int right = query(2 * ind + 2, mid + 1, high, l, r);
+        return left + right;
+    }
+};
+
+
+
+class DisjointSet {
+
+public:
+    vector<int>rank, parent, size;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+
+    }
+
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+
+    }
+
+};
+
+
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n; cin >> n;
+    while (n != 1)
+    {
+        cout << n << " ";
+        if (n % 2 == 0)n /= 2;
+        else n = n * 3 + 1;
+    }
+    cout << 1 << endl;
+
+
+}
